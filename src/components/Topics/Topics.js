83817813b3a09834157e0cb058e8efc6488eb325
rabiul155 <siyamict@gmail.com>
2022-10-11 23:38:06 +0600

    banner added

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import './Topics.css';
 const Topics = () => {
     return (
         <div>
-            <h3>topics page</h3>
+            <div className='banner'>
+                <h1 className=' text-info p-3 fw-bolder'>Welcome to Quiz-Corner</h1>
+                <h3 className=' text-info pb-3 fw-bolder'>Make learning awesome!</h3>
+                <h5 className=' text-light fw-bolder'>It only takes minutes to create a learning game or trivia quiz on any topic, <br /> in any programming language.</h5>
+            </div>
         </div>
     );
 };

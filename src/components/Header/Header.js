0a1a55d1356b89c2f5ca0298a0f4c375c1e8c50f
rabiul155 <siyamict@gmail.com>
2022-10-11 23:50:17 +0600

    customize nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-light">
+            <nav className="navbar navbar-expand-lg nav-color" >
                 <div className="container">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -14,10 +15,10 @@ const Header = () => {
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
                         </ul>
-                        <div className="d-flex justify-content-between">
-                            <Link className='btn btn btn-light me-4 fw-bold' to='/'>Topics</Link>
-                            <Link className='btn btn btn-light me-4 fw-bold' to='/statistics'>Statistics</Link>
-                            <Link className='btn btn btn-light me-4 fw-bold' to='/blog'> Blog</Link>
+                        <div className="d-flex justify-content-between link-btn">
+                            <Link className='btn me-4 fw-bold' to='/'>Topics</Link>
+                            <Link className='btn  me-4 fw-bold' to='/statistics'>Statistics</Link>
+                            <Link className='btn me-4 fw-bold' to='/blog'> Blog</Link>
 
                         </div>
                     </div>
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -4,7 +4,7 @@ const Topics = () => {
     return (
         <div>
             <div className='banner'>
-                <h1 className=' text-info p-3 fw-bolder'>Welcome to Quiz-Corner</h1>
+                <h1 className=' text-info p-4 fw-bolder'>Welcome to Quiz-Corner</h1>
                 <h3 className=' text-info pb-3 fw-bolder'>Make learning awesome!</h3>
                 <h5 className=' text-light fw-bolder'>It only takes minutes to create a learning game or trivia quiz on any topic, <br /> in any programming language.</h5>
             </div>

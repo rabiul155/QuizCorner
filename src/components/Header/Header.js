28fba414c745ce1bea4c40b297e7bdcb1b4f39d2
rabiul155 <siyamict@gmail.com>
2022-10-11 22:45:25 +0600

    name and icon addee

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <Link className="navbar-brand fw-bolder text-primary" href="#">Navbar</Link>
+                    <Link className="navbar-brand fw-bolder text-primary" href="#">Quiz-Corner</Link>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 

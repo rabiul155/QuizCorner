6d9f5b306f430cd234651c769c42c91008ab3b7f
rabiul155 <siyamict@gmail.com>
2022-10-12 14:31:45 +0600

    toast added

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import './Option.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Option = ({ option }) => {
-    console.log(option);
+const Option = ({ id, option, question, correctAnswer }) => {
+
+    console.log(question, id)
+
+
+    const handleClick = (option) => {
+
+        console.log(id);
+
+        if (option === correctAnswer) {
+
+            toast("Right answer")
+        }
+        else {
+
+            toast("Wrong answer")
+        }
+
+
+    }
+
+
     return (
         <div>
-            <p className=' border rounded-3 fw-bold col border-primary p-3'><input type="checkbox" name="" id="" /> {option}</p>
+            <p className={`q-option border rounded-3 fw-bold col border-primary p-3 `}>
+                <input onClick={() => handleClick(option)} className=' me-3' type="radio" name={question} />
+                {option}
+            </p>
+
+            <ToastContainer />
         </div>
     );
 };
--- src/components/TestQuiz/TestQuiz.js
+++ src/components/TestQuiz/TestQuiz.js
@@ -2,17 +2,22 @@ import React from 'react';
 import Option from '../Option/Option';
 
 const TestQuiz = ({ quiz }) => {
-    const { question, options } = quiz;
+    const { id, question, options, correctAnswer } = quiz;
+
     console.log(quiz)
 
+
     return (
-        <div>
-            <h3> {question} </h3>
+        <div className=' border border-2 border-primary p-2 rounded-3 m-4'>
+            <h3 className=' pb-2'> {question} </h3>
             <div className=' row row-cols-2 w-75 m-auto '>
                 {
                     options.map((option, idx) => <Option
                         key={idx}
+                        correctAnswer={correctAnswer}
+                        question={question}
                         option={option}
+                        id={id}
                     ></Option>)
                 }
             </div>
